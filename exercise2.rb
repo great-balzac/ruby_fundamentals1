c325ca3fbe84dadee1c9004749daee96bdcf58a5
Daniel Pereira <pereiradpg@yahoo.ca>
2015-08-17 18:22:25 -0400

    Assignment 1 - B: First commit

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+# This is how I would puts a calculation for a tip on a $55.00 dinner.
+puts "Dinner total: $55.00 plus tip = $#{(55 * 0.2) + 55}"
+
+# This is how I would add a string and an integer:
+puts "100".to_i + 100
+
+# Outputting the product of 45628 and 7839 using string interpolation
+puts "The product of 45628 and 7839 is #{45628*7839}."
+
