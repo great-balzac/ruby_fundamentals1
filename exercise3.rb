ccb410227042e9eb4f198a490116148718801a18
Daniel Pereira <pereiradpg@yahoo.ca>
2015-08-17 22:00:37 -0400

    Tested OK program accepts user's age and outputs year of birth after calculation

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
 name = gets.strip
-puts "Hi #{name.capitalize}!"
+puts "Hi #{name.capitalize}!  What is your age?"
+age = gets.strip
+birthyear = (Time.now.year - age.to_i)
+puts "Your birth year is #{birthyear.to_s}."
